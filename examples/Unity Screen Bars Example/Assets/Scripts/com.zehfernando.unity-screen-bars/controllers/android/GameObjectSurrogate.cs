70e98f33cbc96e4adab04ee4394938b2f6415e11
Zeh Fernando <zeh@zehfernando.com>
2020-01-19 13:44:05 -0500

    Android: game object surrogate also exposes focus gain/lost events

--- examples/Unity Screen Bars Example/Assets/Scripts/com.zehfernando.unity-screen-bars/controllers/android/GameObjectSurrogate.cs
+++ examples/Unity Screen Bars Example/Assets/Scripts/com.zehfernando.unity-screen-bars/controllers/android/GameObjectSurrogate.cs
@@ -4,6 +4,15 @@ namespace com.zehfernando.UnityScreenBars.android {
 	public class GameObjectSurrogate : MonoBehaviour {
 		private static GameObjectSurrogate instance;
 
+		public delegate void SimpleHandler();
+
+		public event SimpleHandler onGainedFocus;
+		public event SimpleHandler onLostFocus;
+
+		private bool hasFocus = true;
+		private bool isPaused = false;
+		private bool lastFocusValue = false;
+
 		public static GameObjectSurrogate getInstance() {
 			if (instance == null) {
 				GameObject gameObject  = new GameObject("AndroidScreenBarsSurrogate");
@@ -15,6 +24,28 @@ namespace com.zehfernando.UnityScreenBars.android {
 
 		void Awake() {
 			DontDestroyOnLoad(this);
+		}
+
+		void OnApplicationFocus(bool newHasFocus) {
+			hasFocus = newHasFocus;
+			updateFocusEvent();
+		}
+
+		void OnApplicationPause(bool newIsPaused) {
+			isPaused = newIsPaused;
+			updateFocusEvent();
+		}
+
+		private void updateFocusEvent() {
+			bool newFocusValue = !isPaused && hasFocus;
+			if (newFocusValue != lastFocusValue) {
+				lastFocusValue = newFocusValue;
+				if (lastFocusValue) {
+					if (onGainedFocus != null) onGainedFocus();
+				} else {
+					if (onLostFocus != null) onLostFocus();
+				}
+			}
 		}
 	}
 }
